ba6fdfe8a7a8e05629c6ec7f52cacac061474d2a
AmanGarg07 <Amangarg7702@gmail.com>
2022-04-12 13:55:30 +0530

    Updates

---
+++ precache-manifest.8d0f55140bd334d5290c7854835e2ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc801aba63d78127ceef5f6d185f8b44",
+    "url": "/Color-Generator/index.html"
+  },
+  {
+    "revision": "12ad048103d232b4413b",
+    "url": "/Color-Generator/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/Color-Generator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/Color-Generator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ad048103d232b4413b",
+    "url": "/Color-Generator/static/js/main.bfca598f.chunk.js"
+  },
+  {
+    "revision": "08f13fa8a24bafc5c908",
+    "url": "/Color-Generator/static/js/runtime-main.a1d42ae7.js"
+  }
+]);
